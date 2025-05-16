d083fe526cbbc48001d13efc080bfd0bc7900cda
Priyanshu <56836660+priyanshuvatsyan@users.noreply.github.com>
2025-05-16 17:17:34 +0530

    added auth popUp

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -2,90 +2,103 @@ import React from 'react'
 import axios from '../api/axios';
 import './styles/Authentication.css'
 import { useState } from 'react'
-import  {useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export default function Authentication() {
 
   const [isLogin, setIsLogin] = useState(true);
-  const [form, setForm] = useState({name:'',email:'',password:''});
-  const navigate = useNavigate(); 
+  const [loading, setloading] = useState(false)
+  const [form, setForm] = useState({ name: '', email: '', password: '' });
+  const navigate = useNavigate();
 
-  const toggleForm = ()=> setIsLogin(!isLogin);
+  const toggleForm = () => setIsLogin(!isLogin);
 
-  const handleChange = (e)=>{
-    setForm({...form,[e.target.name]:e.target.value});
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const url = isLogin ? '/auth/login':'/auth/register';
-      const payload = isLogin? {email:form.email,password:form.password}
-                      :
-                      form;
-      const res = await axios.post(url,payload);
+      setloading(true);
+      const url = isLogin ? '/auth/login' : '/auth/register';
+      const payload = isLogin ? { email: form.email, password: form.password }
+        :
+        form;
+      const res = await axios.post(url, payload);
 
       if (isLogin) {
         localStorage.setItem('token', res.data.token);
         //alert('Welcome user');
         navigate('/');
         //Navigate to home page
-      }else{
+      } else {
         alert('Registration successful');
         setIsLogin(true);
       }
 
     } catch (err) {
       alert(err.response?.data?.message || 'Something went wrong');
+    } finally {
+      setloading(false);
     }
   }
 
-return (
+  return (
     <div className='auth-container' >
       <div className="auth-inside-box">
-      <h2>{isLogin? 'Login':'Register'}</h2>
-      <form className='auth-form' onSubmit={handleSubmit}>
-        {!isLogin && (
-          <input type="text"
-          placeholder='Name'
-          name='name'
-          value={form.name}
-          onChange={handleChange}
-          required
+        <h2>{isLogin ? 'Login' : 'Register'}</h2>
+        <form className='auth-form' onSubmit={handleSubmit}>
+          {!isLogin && (
+            <input type="text"
+              placeholder='Name'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              required
+            />
+          )}
+          <br />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            required
           />
-        )}
-        <br />
-         <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={form.email}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={form.password}
-          onChange={handleChange}
-          required
-        />
-        <br />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+            required
+          />
+          <br />
 
-        <button className="submitbtn">{isLogin?'Login':'Register'}</button>
-      </form>
-      <p onClick={toggleForm}>
-  {isLogin ? (
-    <>Don't have an account? <span>Register</span></>
-  ) : (
-    <>Already have an account? <span>Login</span></>
-  )}
-</p>
+          <button className="submitbtn">{isLogin ? 'Login' : 'Register'}</button>
+        </form>
+        <p onClick={toggleForm}>
+          {isLogin ? (
+            <>Don't have an account? <span>Register</span></>
+          ) : (
+            <>Already have an account? <span>Login</span></>
+          )}
+        </p>
 
       </div>
+     {
+      loading && (
+         <div className="dialog-backdrop">
+        <div className="dialog authdialog ">
+           <p>⏳ Please wait</p>
+        </div>
+      </div>
+      )
+     }
     </div>
   )
 }
